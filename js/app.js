2bfcc37839d8cc3e50744b57003114918f7945cb
wsd <jj@ss.pl>
2018-03-17 15:05:54 +0100

    eventy

---
+++ js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var showMenuDOM = document.querySelector('.for-dropdown');
+    var menuDOM = document.querySelector('.dropdown');
+
+    showMenuDOM.addEventListener('mouseover', function() {
+        menuDOM.style.display = 'block'
+    });
+    
+    showMenuDOM.addEventListener('mouseout', function() {
+        menuDOM.style.display = 'none'
+    });
+
+    // tutaj będziemy umieszczać kod ze wszystkich zadań
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+});
